015cebba2555fa10fbd74153b55e06e2c63f4dc0
Junnosuke Tasaka <junnosuke0120@gmail.com>
2021-08-06 23:49:03 +0900

    add: comments

--- client/src/api.js
+++ client/src/api.js
@@ -5,15 +5,18 @@ async function request(path, options = {}) {
   return response.json();
 }
 
+// レストラン一覧取得
 export async function getRestaurants(arg = {}) {
   const params = new URLSearchParams(arg);
   return request(`/restaurants?${params.toString()}`);
 }
 
+// 特定のレストランを取得
 export async function getRestaurant(restaurantId) {
   return request(`/restaurants/${restaurantId}`);
 }
 
+// レストランのレビューを取得
 export async function getRestaurantReviews(restaurantId, arg = {}) {
   const params = new URLSearchParams(arg);
   return request(`/restaurants/${restaurantId}/reviews?${params.toString()}`);
